1cb20baf51fc84aab87367cfbc206ab9d62de963
Sanya Doda <sanyaadoda@gmail.com>
2025-07-15 16:59:18 +0530

    remove Dups from unsorted linked list

---
+++ ch 2 linked lists/1.cpp
@@ -0,0 +1,54 @@
+// Remove Dups: Write code to remove duplicates from an unsorted linked list. 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public: 
+    int data;
+    Node* next;
+    Node(int val){
+        data=val;
+        next=nullptr;
+    }
+};
+// brute - tc: O(n^2) sc: O(1) - TLE
+Node* removeDupsUnsorted(Node* head){
+    Node* temp=head;
+    while(temp!=NULL){
+        Node* one=temp;
+        while(one->next){
+            if(one->next->data==temp->data){
+                Node* del=one->next;
+                one->next=one->next->next;
+                delete del;
+            }else{
+                one=one->next;
+            }
+        }
+        temp=temp->next;
+    }
+    return head;
+}
+
+void printList(Node* head){
+    Node* curr=head;
+    while(curr!=NULL){
+        cout<<curr->data<<"->";
+        curr=curr->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    Node* head=new Node(1);
+    head->next=new Node(2);
+    head->next->next=new Node(1);
+    head->next->next->next=new Node(3);
+    head->next->next->next->next=new Node(2);
+    head->next->next->next->next->next=new Node(4);
+    printList(head);
+    head=removeDupsUnsorted(head);
+    printList(head);
+    return 0;
+}
+
